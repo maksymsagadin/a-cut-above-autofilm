4c4cde28a5e0392f229b31912977735b236c54e2
Maksym Sagadin <maxsagadin@gmail.com>
2022-04-12 16:58:54 -0700

    updated carousel speed and transition

--- components/FeaturedPostCard.jsx
+++ components/FeaturedPostCard.jsx
@@ -6,9 +6,9 @@ import Link from 'next/link'
 const FeaturedPostCard = ({ post }) => (
   <div className="relative h-96">
     <div className="absolute rounded-lg bg-center bg-no-repeat bg-cover shadow-md inline-block w-full h-96" style={{ backgroundImage: `url('${post.featuredImage.url}')` }} />
-    <div className="flex flex-col rounded-lg p-4 items-end absolute w-full h-full">
-        <p className="text-gray-200 mb-4 text-shadow font-semibold text-sm">{moment(post.createdAt).format('MMM DD, YYYY')}</p>
-        <p className="text-gray-200 mb-4 text-shadow font-semibold text-xs text-right">{post.title}</p>
+    <div className="flex flex-col rounded-lg p-6 items-end justify-end absolute w-full h-full">
+        <p className="text-gray-200 text-shadow font-semibold text-sm">{moment(post.createdAt).format('MMM DD, YYYY')}</p>
+        <p className="text-gray-200 text-shadow font-semibold text-sm">{post.title}</p>
     </div>
     <div className="absolute rounded-lg bg-center bg-gradient-to-b opacity-40 hover:bg-none from-gray-400 via-gray-700 to-black w-full h-96">
         <Link href={`/post/${post.slug}`}><span className="cursor-pointer absolute w-full h-full" /></Link>
