72f79957b9b25bfb17bbf9dee6270d6c07d43fb9
Maksym Sagadin <maxsagadin@gmail.com>
2022-04-12 12:52:53 -0700

    removed arrows

--- sections/FeaturedPosts.jsx
+++ sections/FeaturedPosts.jsx
@@ -37,7 +37,7 @@ const FeaturedPosts = () => {
   }, [])
 
   const customLeftArrow = (
-    <div className="absolute sm:hidden left-0 py-3 cursor-pointer ">
+    <div className="absolute hidden left-0 py-3 cursor-pointer ">
       <svg xmlns="http://www.w3.org/2000/svg" className="h-6 text-gray-200 w-full" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" d="M12 0c6.623 0 12 5.377 12 12s-5.377 12-12 12-12-5.377-12-12 5.377-12 12-12zm0 1c6.071 0 11 4.929 11 11s-4.929 11-11 11-11-4.929-11-11 4.929-11 11-11zm3 5.753l-6.44 5.247 6.44 5.263-.678.737-7.322-6 7.335-6 .665.753z" />
       </svg>
@@ -45,7 +45,7 @@ const FeaturedPosts = () => {
   )
 
   const customRightArrow = (
-    <div className="absolute sm:hidden right-0 cursor-pointer">
+    <div className="absolute hidden cursor-pointer">
       <svg  xmlns="http://www.w3.org/2000/svg" className="h-6 text-gray-200 w-full" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" d="M12 0c6.623 0 12 5.377 12 12s-5.377 12-12 12-12-5.377-12-12 5.377-12 12-12zm0 1c6.071 0 11 4.929 11 11s-4.929 11-11 11-11-4.929-11-11 4.929-11 11-11zm-3 5.753l6.44 5.247-6.44 5.263.678.737 7.322-6-7.335-6-.665.753z" />
       </svg>
@@ -54,7 +54,7 @@ const FeaturedPosts = () => {
 
   return (
     <div className="mb-8">
-      <h3 className='text-gray-200 px-4 text-4xl font-semibold'></h3>
+      <h3 className='text-gray-200 text-4xl font-semibold'></h3>
       <Carousel infinite customLeftArrow={customLeftArrow} customRightArrow={customRightArrow} responsive={responsive} itemClass="px-4">
         {dataLoaded && featuredPosts.map((post, index) => (
           <FeaturedPostCard key={index} post={post} />
