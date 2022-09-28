5c51693b3953d28de07acacca73569cc43e1dbc1
Maksym Sagadin <maxsagadin@gmail.com>
2022-09-27 17:56:04 -0700

    moved blog to seperate page

---
+++ pages/blog.js
@@ -0,0 +1,34 @@
+import Head from 'next/head'
+import { PostCard, Categories, PostWidget } from '../components'
+import { getPosts } from '../services'
+import { FeaturedPosts } from '../sections'
+
+
+export default function Home({ posts }) {
+  return (
+    <div className="container mx-auto px-2 lg:mb-10">
+      <FeaturedPosts />
+      <div className='grid lg:grid-cols-12 grid-cols-1 lg:gap-8'>
+        {/* Left Section */}
+        <div className='lg:col-span-8 col-span-1'>
+          {posts.map((post) => <PostCard post={post.node} key={post.node.title} />)}
+        </div>
+        {/* Right Section */}
+        <div className='col-span-1 lg:col-span-4'>
+            <div className='lg:sticky relative text-gray-200'>
+              <PostWidget />
+              <Categories />
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export async function getStaticProps() {
+  const posts = (await getPosts()) || []
+  
+  return {
+    props: { posts }
+  }
+}
