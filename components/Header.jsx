f1dd49cba0c0e0c75b5ac3cb3ddabe8a58b907b3
Maksym Sagadin <maxsagadin@gmail.com>
2022-10-17 16:01:50 -0700

    updated navbar links for smaller screen sizes

--- components/Header.jsx
+++ components/Header.jsx
@@ -18,27 +18,27 @@ const Header = () => {
     return (
         <div className='container bg-black sticky top-0 z-10 mx-auto px-2 rounded-xl bg-opacity-50 lg:px-8 '>
             <div className='absolute top-0 left-0 lg:px-8 px-4 w-full inline-block my-4 py-2'>
-                <div className='md:float-left block'>
+                <div className='sm:float-left block'>
                     <Link href='/'>
                         <span className='cursor-pointer  hover: inline-block transition duration-500 transform hover:translate-x-4'>
                             <Image src={logo} height={64} width={64} />
                         </span>
                     </Link>
                 </div>
-                <div className='hidden md:float-left md:contents'>
+                <div className='hidden sm:contents'>
                     <Link href='/blog'>
-                        <span className='md:float-right text-3xl p-4 font-semibold cursor-pointer text-gray-200 hover:text-blue-300 inline-block transition duration-500 transform hover:translate-y-2'>
+                        <span className='sm:float-right text-3xl p-4 font-semibold cursor-pointer text-gray-200 hover:text-blue-300 inline-block transition duration-500 transform hover:translate-y-2'>
                             Blog
                         </span>
                     </Link>
                     <Link href='/'>
-                        <span className='md:float-right text-3xl p-4 font-semibold cursor-pointer text-gray-200 hover:text-blue-300 inline-block transition duration-500 transform hover:translate-y-2'>
+                        <span className='sm:float-right text-3xl p-4 font-semibold cursor-pointer text-gray-200 hover:text-blue-300 inline-block transition duration-500 transform hover:translate-y-2'>
                             Home
                         </span>
                     </Link>
                 </div>
             </div>
-            <div className='hidden lg:flex fixed flex-col bottom-[5%] left-0'>
+            <div className='hidden sm:flex fixed flex-col bottom-[5%] left-0'>
                 <ul>
                     <li className='w-[160px] h-[60px] flex px-4 items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
                         <a
