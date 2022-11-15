ebc07df1e77f870939c2c002f5ef4f33fa71f805
Maksym Sagadin <maxsagadin@gmail.com>
2022-11-14 16:53:03 -0800

    added Link to blog post per photo

--- components/FeaturedImage.jsx
+++ components/FeaturedImage.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import Link from 'next/link'
 
 const FeaturedImage = ({ post }) => {
   return (
-    <>
-        <div className=' bg-center relative bg-no-repeat bg-cover w-[350px] h-[500px]' style={{ backgroundImage: `url('${post.featuredImage.url}')` }} >
-          <p className='h-full w-full flex justify-center items-center opacity-0 hover:opacity-50 bg-black text-[#d7e1f3]'>{post.title}</p>
+    <Link  href={`/post/${post.slug}`}>
+        <div className='cursor-pointer bg-center relative bg-no-repeat bg-cover w-[350px] h-[500px]' style={{ backgroundImage: `url('${post.featuredImage.url}')` }} >
+          <p className='cursor-pointer h-full w-full flex justify-center items-center opacity-0 hover:opacity-50 bg-black text-[#d7e1f3]'>{post.title}</p>
         </div>
-    </>
+    </Link>
   )
 }
 
