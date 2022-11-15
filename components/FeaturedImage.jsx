d2522714255ea4b66caa2456d0087df4cb264a2f
Maksym Sagadin <maxsagadin@gmail.com>
2022-11-14 16:15:36 -0800

    used div w/ background image from url

--- components/FeaturedImage.jsx
+++ components/FeaturedImage.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 
-const FeaturedImage = ({post}) => {
+const FeaturedImage = ({ post }) => {
   return (
     <>
         <div className=' bg-center relative bg-no-repeat bg-cover w-[350px] h-[500px]' style={{ backgroundImage: `url('${post.featuredImage.url}')` }} >
-          {/* <img src={`url('${post.featuredImage.url}')`} alt={post.title} /> */}
           <p className='h-full w-full flex justify-center items-center opacity-0 hover:opacity-50 bg-black text-[#d7e1f3]'>{post.title}</p>
         </div>
     </>
