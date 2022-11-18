164927bcd4620b61f9ce9377ecb44203b86d5218
Maksym Sagadin <maxsagadin@gmail.com>
2022-11-17 16:11:55 -0800

    small update to try and make img work.

--- components/FeaturedImage.jsx
+++ components/FeaturedImage.jsx
@@ -3,8 +3,9 @@ import Link from 'next/link'
 
 const FeaturedImage = ({ post }) => {
   return (
-    <Link  href={`/post/${post.slug}`}>
-        <div className='cursor-pointer bg-center relative bg-no-repeat bg-cover w-[350px] h-[500px]' style={{ backgroundImage: `url('${post.featuredImage.url}')` }} >
+    <Link href={`/post/${post.slug}`}>
+        <div className='cursor-pointer bg-center bg-no-repeat bg-cover w-[250px] h-[400px] lg:w-[400px] lg:h-[550px]' style={{ backgroundImage: `url('${post.featuredImage.url}')` }} >
+          {/* <img src={`${post.featuredImage.url}`} alt={`${post.alt}`} className='h-full' /> */}
           <p className='cursor-pointer h-full w-full flex justify-center items-center opacity-0 hover:opacity-50 bg-black text-[#d7e1f3]'>{post.title}</p>
         </div>
     </Link>
@@ -12,3 +13,4 @@ const FeaturedImage = ({ post }) => {
 }
 
 export default FeaturedImage
+
