b1a3885f20f8c5bd25b0fa10bf9484eefb2fc5da
Maksym Sagadin <maxsagadin@gmail.com>
2022-11-17 16:25:20 -0800

    small styling changes

--- sections/About.jsx
+++ sections/About.jsx
@@ -4,19 +4,19 @@ import img from '../public/spray.jpg'
 
 const About = () => {
   return (
-    <div name='about' className='w-full h-full py-8 relative text-gray-300'>
-      <div className='flex flex-col justify-center items-center px-8 '>
-        <div className='pb-8 pl-8'>
-            <p className='text-4xl font-bold inline border-b-4 border-yellow-600'>
+    <div name='about' className='w-full h-full py-8 mt-8 relative text-gray-300'>
+      <div className='flex flex-col justify-center md:items-center px-8 '>
+        <div className='pb-8 px-2'>
+            <p className='text-4xl px-2 font-bold border-b-4 border-yellow-600'>
               About
             </p>
         </div>
-        <div className='grid sm:grid-cols-2 gap-3 px-2'>
-            <div className=''>
-              <Image src={img} objectFit={'contain'} className='opacity-75 ' />
+        <div className='grid md:grid-cols-2 gap-6 justify-around px-2'>
+            <div className='py-4'>
+              <Image src={img} objectFit={'contain'} className='opacity-75 rounded' />
             </div>
-            <div className='flex justify-center items-center pr-10'>
-              <p className='xs:text-center sm:text-left'>
+            <div className='flex justify-center items-center'>
+              <p className='text-left'>
                 Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 <br/>
                 <br/>
