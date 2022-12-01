516d267e9ac9e18039de47eb422cbf5531ec447f
Maksym Sagadin <maxsagadin@gmail.com>
2022-11-30 17:17:05 -0800

    updated margin top for Header

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -11,7 +11,7 @@ const CategoryPost = ({ posts }) => {
         return <Loader />
     }
     return (
-    <div className="container mx-auto px-10 mb-8">
+    <div className="mx-auto px-10 mt-28 mb-8">
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="col-span-1 lg:col-span-8">
           {posts.map((post, index) => (
