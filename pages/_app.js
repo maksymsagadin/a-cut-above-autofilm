32cc5e974788556324b1a38e4f5f789903093649
Maksym Sagadin <maxsagadin@gmail.com>
2022-11-30 17:17:22 -0800

    added Meta data across app

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from 'react';
-import { Layout } from '../components';
+import React, { useEffect, useState } from 'react'
+import Head from 'next/head'
+import { Layout } from '../components'
 
 import 'tailwindcss/tailwind.css'
 import '../styles/globals.scss'
@@ -7,6 +8,11 @@ import '../styles/globals.scss'
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <title>A Cut Above Autofilm</title>
+        <meta name="description" content="Paint Protection Film" />
+        <link rel="icon" href="/knife-color.png" />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   )
