278d9aaedc007860a04a0653846db68f88b3f33d
Maksym Sagadin <maxsagadin@gmail.com>
2022-12-01 17:55:56 -0800

    updated button color styling

--- sections/HeroSection.jsx
+++ sections/HeroSection.jsx
@@ -13,11 +13,11 @@ const HeroSection = () => {
         <h2 className='text-2xl sm:text-6xl font-bold text-[#d7e1f3]'>
           Protection like no other.
         </h2>
-        <p className='py-4 max-w-[600px] text-[#eaf1f9]'>
+        <p className='py-4 max-w-[600px]'>
         We have an extensive range of stunning, unique design options, the STEK DYNOseries redefines the possibilities of an automotive customization.
         </p>
         <div className='flex flex-row'>
-          <button className='border-2 rounded px-6 py-3 my-2 ml-4 flex items-center hover:bg-[#3378ca] hover:border-[#3378ca]'>
+          <button className='border-2 rounded bg-[#3378ca] bg-opacity-70 px-6 py-3 my-2 ml-4 flex items-center hover:bg-opacity-100 hover:border-[#3378ca]'>
             <LinkScroll to='about' offset={-15} smooth='true' className='text-[#eaf1f9]'>Get To Know Us</LinkScroll>
           </button>
         </div>
@@ -26,7 +26,7 @@ const HeroSection = () => {
         <ul>
           <li className='w-[160px] h-[60px] flex px-4 items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
             <a
-              className='flex justify-between items-center w-full  text-gray-300'
+              className='flex justify-between items-center w-full'
               href='https://www.instagram.com/acutaboveautofilms/'
               target='_blank'
               rel="noreferrer"
@@ -36,7 +36,7 @@ const HeroSection = () => {
           </li>
           <li className='w-[160px] h-[60px] flex px-4 items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
             <a
-              className='flex justify-between items-center w-full text-gray-300'
+              className='flex justify-between items-center w-full'
               href='mailto:ACutAboveAutoFilms@gmail.com'
               target='_blank'
               rel="noreferrer"
@@ -50,5 +50,3 @@ const HeroSection = () => {
   )
 }
 export default HeroSection
-
-// Le mans color code - text-[#336c98] or #3378ca
