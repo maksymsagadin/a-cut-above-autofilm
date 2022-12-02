b4bc58bbd5c119800bdd1a820ccc7864ad4d8177
Maksym Sagadin <maxsagadin@gmail.com>
2022-12-02 11:45:51 -0800

    small update

--- components/Loader.jsx
+++ components/Loader.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 
 const Loader = () => (
     <div className="text-center">
@@ -14,6 +14,6 @@ const Loader = () => (
             Loading
         </button>
     </div>
-);
+)
 
-export default Loader;
+export default Loader
