cb64ed4d06fb5dc2c88391c6b9d3c1febfdb98f1
Maksym Sagadin <maxsagadin@gmail.com>
2022-12-08 16:09:54 -0800

    moved location of Post Widget

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -14,15 +14,15 @@ const PostDetails = ({ post }) => {
     return (
         <div className='mx-auto mt-28 px-4 sm:px-8 mb-8'>
             <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
-                <div className='col-span-1 lg:col-span-8'>
+                <div className='col-span-1 lg:col-span-9'>
                     <PostDetail post={post} />
                     <Author author={post.author} />
                     <CommentsForm slug={post.slug} />
                     <Comments slug={post.slug} />
+                    <PostWidget slug={post.slug} categories={post.categories.map((category) => category.slug)} />
                 </div>
-                <div className='col-span-1 lg:col-span-4'>
+                <div className='col-span-1 lg:col-span-3'>
                     <div className='relative lg:sticky top-8'>
-                        <PostWidget slug={post.slug} categories={post.categories.map((category) => category.slug)} />
                         <Categories />
                     </div>
                 </div>
