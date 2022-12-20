1bca8a6ef28595fb7492cef334948e8221aea430
Maksym Sagadin <maxsagadin@gmail.com>
2022-12-19 17:26:14 -0800

    updated about section content

--- sections/About.jsx
+++ sections/About.jsx
@@ -7,22 +7,18 @@ const About = () => {
     <div id='about' className='w-full h-full py-8 mt-8 relative'>
       <div className='flex flex-col justify-center md:items-center px-8 '>
         <div className='pb-8 px-2'>
-            <p className='text-4xl px-2 font-bold border-b-4 border-yellow-600'>
+            <p className='text-4xl px-2 font-bold border-b-4 border-yellow-400'>
               About
             </p>
         </div>
-        <div className='grid md:grid-cols-2 gap-8 justify-around px-2 mt-4'>
-            <div>
+        <div className='grid md:grid-cols-2 gap-8 md:gap-12 justify-center px-2 md:px-16 lg:px-24 mt-4'>
+            <div className='flex justify-center'>
               <Image src={img} objectFit={'contain'} className='opacity-75 rounded' />
             </div>
-            <div className='flex flex-col items-center indent-4 text-left'>
+            <div className='flex flex-col items-center indent-4 text-justify text-lg lg:text-2xl'>
               <p>
-                What started off as a part time job vinyl wrapping at SFG Wraps while I was finishing school, turned into a fine art of heating and stretching vinyl to it's limits, learning the hard way how to take apart cars from the bumpers to the doors to even the small little trim pieces that often break upon removal.
+                A Cut Above was created out of the need for a high quality vinyl wrap and paint protection film install shop in the Livermore area. We hold ourselves accountable to my Nona's motto, "Whether the job be big or small do it right or not at all." We only offer services that we excel at and we're always learning to expand that list. Wrapping is an experimental art, not an exact science and we like pushing our ability and testing the limits of film!
               </p>
-              <br/>
-              <p>
-                With years of experience working with Vinyl, I decided to take on the next toughest challenge: shaping thick PPF to become the shape of a car. While working with PPF can sometimes feel like it's more difficult, the results always leave me blown away and excited to start the next job!
-              </p> 
             </div>
         </div>
       </div>
