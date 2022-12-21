1b4580959feb342f0baaf2d45a358c2908a25857
Maksym Sagadin <maxsagadin@gmail.com>
2022-12-21 13:05:42 -0800

    updated address information with icons

--- sections/FindUs.jsx
+++ sections/FindUs.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import Image from 'next/image'
-import img from '../public/test.png'
+import { HiOutlinePhone } from 'react-icons/hi'
+import { HiOutlineMail } from 'react-icons/hi'
+import { FaInstagram } from 'react-icons/fa'
+import { HiOutlineOfficeBuilding } from 'react-icons/hi'
+import img from '../public/workshop-m3.png'
 
 const FindUs = () => {
     return (
@@ -11,21 +15,22 @@ const FindUs = () => {
                     Find Us
                     </p>
                 </div>
-                <div className='grid md:grid-cols-2 md:order-last gap-8 lg:gap-16 justify-center px-2 md:px-4 lg:px-8 xl:px-16 mt-4'>
+                <div className='grid md:grid-cols-2 md:order-last gap-8 lg:gap-16 justify-center md:px-4 lg:px-8 xl:px-16 mt-4'>
                     <div className='flex justify-center'>
                         <Image src={img} optimize objectFit={'contain'} className='rounded' />
                     </div>
-                    <div className='flex flex-col relative justify-around md:-order-1 text-justify text-lg lg:text-2xl'>
+                    <div className='flex flex-col relative justify-around md:-order-1 lg:text-2xl'>
                         <p className='mb-8'>
-                            Feel free to reach out with any questions you may have, we look forward to working with you! 
+                            Reach out with any questions you may have, we look forward to working with you! 
                         </p>
-                        <address className='text-left text-xl sm:text-lg text-[#d7e1f3]'>
-                            Phone: <a href='tel:9252649707' >925-264-9709</a><br />
-                            Email: <a href='mailto:ACutAboveAutoFilms@gmail.com'  target='_blank' rel='noreferrer'>ACutAboveAutoFilms@gmail.com</a><br />
-                            Instagram: <a href='https://www.instagram.com/acutaboveautofilms/' target='_blank' rel='noreferrer' >@acutaboveautofilms</a><br />
-                            <br />
-                            Address: Modified Motorsports, LLC.<br />
-                            318 Lindbergh Ave, Livermore, CA<br />
+                        <address className='text-left text-base md:text-lg lg:text-xl text-[#d7e1f3]'>
+                            <a href='tel:9252649707' className='flex items-center tracking-widest py-2'><HiOutlinePhone size={30}/> : 925-264-9709</a>
+                            <a href='mailto:ACutAboveAutoFilms@gmail.com'  target='_blank' rel='noreferrer' className='flex whitespace-nowrap items-center py-2'><HiOutlineMail size={30}/> : ACutAboveAutoFilms@gmail.com</a>
+                            <a href='https://www.instagram.com/acutaboveautofilms/' target='_blank' rel='noreferrer' className='flex whitespace-nowrap items-center tracking-widest py-2'><FaInstagram size={30}/> : @ACutAboveAutoFilms</a>
+                            <div className='flex whitespace-nowrap'>
+                                <HiOutlineOfficeBuilding size={30}/>: Modified Motorsports, LLC.<br />
+                                318 Lindbergh Ave, Livermore, CA<br />
+                            </div>
                         </address>
                     </div>
                 </div>
