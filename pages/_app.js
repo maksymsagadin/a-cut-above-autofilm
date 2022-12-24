e049137bb91edc00c37c6d8b2ad8ac3c6ac4ae85
Maksym Sagadin <maxsagadin@gmail.com>
2022-12-24 13:22:20 -0800

    linked google font into Head

--- pages/_app.js
+++ pages/_app.js
@@ -17,6 +17,9 @@ function MyApp({ Component, pageProps }) {
         <meta property="og:title" content="A Cut Above Autofilm" />
         <meta property="og:description" content="Paint Protection Film by those who care!" />
         <link rel="icon" href="/knife-color.png" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,300;0,400;0,500;0,600;0,800;1,700&display=swap" rel="stylesheet" />
       </Head>
       <Component className='font-raleway' {...pageProps} />
     </Layout>
