62726de657e31d2dfd86465c0c84aa4e8acd747c
Maksym Sagadin <maxsagadin@gmail.com>
2022-12-24 13:16:23 -0800

    added font class to inherited components

--- pages/_app.js
+++ pages/_app.js
@@ -11,14 +11,14 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <title>A Cut Above Autofilm</title>
         <meta charSet="UTF-8"/>
-        <meta name='keywords' content='Paint Protection Film, PPF, Vinyl'/>
+        <meta name='keywords' content='Paint Protection Film, PPF, Vinyl, A, Cut, Above, Wrap'/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="Paint Protection Film by those who care!" />
         <meta property="og:title" content="A Cut Above Autofilm" />
         <meta property="og:description" content="Paint Protection Film by those who care!" />
         <link rel="icon" href="/knife-color.png" />
       </Head>
-      <Component {...pageProps} />
+      <Component className='font-raleway' {...pageProps} />
     </Layout>
   )
 }
