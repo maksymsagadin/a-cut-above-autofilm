3a75b15e92b0434e9c03881430b67916a7f1691c
Maksym Sagadin <maxsagadin@gmail.com>
2022-12-24 06:34:27 -0800

    updated styling

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -77,23 +77,23 @@ const PostDetail = ({ post }) => {
                     className='object-top h-full w-full rounded-t-lg'
                 />
             </div>
-            <div className='px-4 lg:px-0'>
-                <div className='flex items-center mb-4 w-full'>
-                    <div className='flex items-center mb-4 lg:mb-0 w-full lg:w-auto mr-8'>
+            <div className='px-2 '>
+                <div className='flex items-center mb-8 w-full'>
+                    <div className='flex items-center lg:mb-0 w-full lg:w-auto mr-8'>
                         <img 
                             alt={post.author.name}
                             src={post.author.photo.url}
-                            height='30px'
-                            width='30px'
-                            className='align-middle rounded-full'
+                            height='50px'
+                            width='50px'
+                            className='rounded-full'
                             />
-                        <p className='align-middle inline text-sm ml-2'>{post.author.name}</p>
+                        <p className='text-lg ml-4'>{post.author.name}</p>
                     </div>
-                    <div className='font-medium'>
-                        <svg xmlns="http://www.w3.org/2000/svg" className='h-6 w-6 inline mr-2 text-gray-500' fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <div className='font-medium flex items-center'>
+                        <svg xmlns="http://www.w3.org/2000/svg" className='h-6 w-6  text-gray-500' fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
                         </svg>
-                        <span className='align-middle text-sm'>
+                        <span className='text-lg  whitespace-nowrap'>
                             {moment(post.createdAt).format('MMM DD, YYYY')}
                         </span>
                     </div>
