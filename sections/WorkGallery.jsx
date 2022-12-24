46447d10256712553c0f10f39aa5796d6d365f0f
Maksym Sagadin <maxsagadin@gmail.com>
2022-12-24 06:32:47 -0800

    updated text sizing

--- sections/WorkGallery.jsx
+++ sections/WorkGallery.jsx
@@ -19,23 +19,23 @@ const WorkGallery = ({ posts }) => {
         <div className='relative overflow-hidden flex flex-col gap-8 lg:flex-row py-8 px-8 lg:pr-0'>
             {/* Left Column */}
             <div className='flex flex-col items-start justify-center w-full lg:max-w-[33%]'>
-                <h2 className='text-2xl sm:text-4xl font-bold text-[#d7e1f3]'>
+                <h2 className='text-2xl md:text-4xl font-bold text-[#d7e1f3]'>
                     A Cut Above.
                 </h2>
-                <h1 className='text-3xl sm:text-5xl font-bold text-yellow-400'>
+                <h1 className='text-2xl md:text-5xl font-bold text-yellow-400'>
                     Protection like no other.
                 </h1>
                 <p className='py-4 max-w-[600px] md:text-lg text-justify'>
                     We understand as fellow enthusiasts that you want to make sure your handing your keys to someone you can trust, that's why we recommend you spend some time checking out our work.
                 </p>
                 <Link href='/blog'>
-                    <button className='border-2 rounded px-6 py-3 my-1 transition duration-500 hover:bg-[#3378ca] hover:border-[#3378ca]'>
+                    <button className='border-2 rounded-lg px-6 py-3 my-1 transition duration-500 hover:bg-[#3378ca] hover:border-[#3378ca]'>
                         View Our Work
                     </button>
                 </Link>
             </div>
             {/* Right Column / Carousel */}
-            <div className='h-full relative rounded overflow-hidden mx-auto w-full lg:max-w-[66%] lg:my-16'>
+            <div className='h-full relative rounded-lg overflow-hidden mx-auto w-full lg:max-w-[66%] lg:my-16'>
                 <div className='flex no-scrollbar relative overflow-x-scroll' ref={scrollRef}>
                 {posts && [posts[0], posts[1], posts[2], posts[3], posts[4]].map((post, index) => (
                     <div key={index}>
