07db618b8d25420d08ad32d202f4a22443670918
Maksym Sagadin <maxsagadin@gmail.com>
2022-12-24 18:11:53 -0800

    updated Navlinks for mobile responsiveness

--- components/Header.jsx
+++ components/Header.jsx
@@ -9,41 +9,41 @@ const Header = () => {
     const router = useRouter()
     return (
         <div className='container top-0'>
-            <div className='absolute top-0 left-0 sm:px-8 px-4 w-full py-4'>
-                <div className='float-left block'>
+            <div className='absolute top-0 left-0 md:px-8 px-4 w-full py-4'>
+                <div className='float-left'>
                     <Link href='/'>
                         <span className='cursor-pointer duration-500 ease-linear inline-block hover:-translate-x-4'>
                             <Image src={logo} height={64} width={64} />
                         </span>
                     </Link>
                 </div>
-                <div className='content'>
+                <div className='container'>
                     {router.pathname == '/' && 
                     <Link href='/blog'>
-                        <span className='float-right sm:text-2xl p-4 font-semibold cursor-pointer text-gray-200 hover:text-blue-300 inline-block transition duration-500 transform hover:translate-y-2'>
+                        <span className='float-right text-lg p-1 md:text-2xl md:p-4 font-semibold cursor-pointer text-gray-200 hover:text-blue-300 inline-block transition duration-500 transform hover:translate-y-2'>
                             Blog
                         </span>
                     </Link> }
                     {router.pathname == '/' && 
                     <LinkScroll to='findus' offset={-15} smooth='true'>
-                        <span className='float-right sm:text-2xl p-4 font-semibold cursor-pointer text-gray-200 hover:text-blue-300 inline-block transition duration-500 transform hover:translate-y-2'>
+                        <span className='float-right text-lg p-1 md:text-2xl md:p-4 font-semibold cursor-pointer text-gray-200 hover:text-blue-300 inline-block transition duration-500 transform hover:translate-y-2'>
                             Find Us
                         </span>
                     </LinkScroll>}
                     {router.pathname == '/' && 
                     <LinkScroll to='services' offset={-15} smooth='true'>
-                        <span className='float-right sm:text-2xl p-4 font-semibold cursor-pointer text-gray-200 hover:text-blue-300 inline-block transition duration-500 transform hover:translate-y-2'>
+                        <span className='float-right text-lg p-1 md:text-2xl md:p-4 font-semibold cursor-pointer text-gray-200 hover:text-blue-300 inline-block transition duration-500 transform hover:translate-y-2'>
                             Services
                         </span>
                     </LinkScroll>}
                     {router.pathname == '/' && 
                     <LinkScroll to='about' offset={-15} smooth='true'>
-                        <span className='float-right sm:text-2xl p-4 font-semibold cursor-pointer text-gray-200 hover:text-blue-300 inline-block transition duration-500 transform hover:translate-y-2'>
+                        <span className='float-right text-lg p-1 md:text-2xl md:p-4 font-semibold cursor-pointer text-gray-200 hover:text-blue-300 inline-block transition duration-500 transform hover:translate-y-2'>
                             About
                         </span>
                     </LinkScroll>}
                     {router.pathname != '/' && <Link href='/'>
-                        <span className='float-right sm:text-2xl p-4 font-semibold cursor-pointer text-gray-200 hover:text-blue-300 inline-block transition duration-500 transform hover:translate-y-2'>
+                        <span className='float-right text-lg p-1 md:text-2xl md:p-4 font-semibold cursor-pointer text-gray-200 hover:text-blue-300 inline-block transition duration-500 transform hover:translate-y-2'>
                             Home
                         </span>
                     </Link>}
@@ -54,9 +54,3 @@ const Header = () => {
 }
 
 export default Header
-
-// <LinkScroll to='about' offset={-15} smooth='true'>
-                    //     <span className='float-right text-3xl p-4 font-semibold cursor-pointer text-gray-200 hover:text-blue-300 inline-block transition duration-500 transform hover:translate-y-2'>
-                    //         About
-                    //     </span>
-                    // </LinkScroll>
