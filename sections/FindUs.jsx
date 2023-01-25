cb7eca7e6b279908ad00ffecae3b8863fcf93f88
Maksym Sagadin <maxsagadin@gmail.com>
2023-01-25 14:51:28 -0800

    updated number to work for international callers.

--- sections/FindUs.jsx
+++ sections/FindUs.jsx
@@ -24,7 +24,7 @@ const FindUs = () => {
                             Reach out with any questions you may have, we look forward to working with you! 
                         </p>
                         <address className='text-left text-base md:text-lg lg:text-xl text-[#d7e1f3]'>
-                            <a href='tel:9252649707' className='flex items-center tracking-widest py-2'><HiOutlinePhone size={30}/>&nbsp;: 925-264-9709</a>
+                            <a href='tel:+1-925-264-9709' className='flex items-center tracking-widest py-2'><HiOutlinePhone size={30}/>&nbsp;: 925-264-9709</a>
                             <a href='mailto:ACutAboveAutoFilms@gmail.com'  target='_blank' rel='noopener noreferrer' className='flex whitespace-nowrap items-center py-2'><HiOutlineMail size={30}/>&nbsp;: ACutAboveAutoFilms@gmail.com</a>
                             <a href='https://www.instagram.com/acutaboveautofilms/' target='_blank' rel='noopener noreferrer' className='flex whitespace-nowrap items-center tracking-wider py-2'><FaInstagram size={30}/>&nbsp;: @ACutAboveAutoFilms</a>
                             <div className='flex tracking-wider py-2'>
