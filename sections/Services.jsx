f1df90a48868815bb98caeb83d1d45f2bafd508b
Maksym Sagadin <maxsagadin@gmail.com>
2023-01-26 22:17:48 -0800

    changed element back

--- sections/Services.jsx
+++ sections/Services.jsx
@@ -6,17 +6,17 @@ const Services = ({ data }) => {
     return (
         <SectionHeadline title='Services' id='services'>
             {/* Center Text */}
-            <div className='flex justify-center text-lg lg:text-2xl indent-4 text-justify md:text-center md:w-2/3 mx-auto my-4 md:my-8 lg:my-12 '>
+            <div className='flex justify-center text-lg lg:text-2xl indent-4 text-justify md:text-center md:w-2/3 mx-auto mb-4 md:mt-4 md:mb-8 lg:mt-8 lg:mb-12 '>
                 <p>{greeting}</p>
             </div>
-            <section className='grid md:grid-cols-3 gap-8 lg:gap-10  md:px-4 lg:px-8 xl:px-16 mt-4 text-center'>
+            <div className='grid md:grid-cols-3 gap-8 lg:gap-10  md:px-4 lg:px-8 xl:px-16 mt-4 text-center'>
                 {services.map((service) => (
                     <div key={service.serviceTitle} className='sm:text-lg lg:text-2xl border-2 border-yellow-400 p-4 rounded-lg'>
                         <p className='mb-2 font-bold'>{service.serviceTitle}</p>
                         <span className='text-base'>{service.serviceDescription}</span>
                     </div>
                 ))}
-            </section>
+            </div>
         </SectionHeadline>
     )
 }
