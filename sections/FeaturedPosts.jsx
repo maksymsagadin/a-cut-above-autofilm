7e38d1a9abbe5d1fab8e370a674bd1cc5e2b9af3
Maksym Sagadin <maxsagadin@gmail.com>
2023-02-23 16:42:55 -0800

    removed uneccesary import

--- sections/FeaturedPosts.jsx
+++ sections/FeaturedPosts.jsx
@@ -3,6 +3,7 @@ import Carousel from 'react-multi-carousel'
 import 'react-multi-carousel/lib/styles.css'
 
 import { FeaturedPostCard } from '../components'
+import { getFeaturedPosts } from '../services'
 
 const responsive = {
   superLargeDesktop: {
