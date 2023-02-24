867657feeffdd3096382a1a362f36af0d2020560
Maksym Sagadin <maxsagadin@gmail.com>
2023-02-24 12:30:23 -0800

    updated image sizes and linked them to the post

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -6,12 +6,14 @@ const PostCard = ({ post }) => {
   
   return (
     <div className='bg-gray-200 bg-opacity-10 text-gray-200 shadow-lg rounded-lg p-0 pb-8  lg:p-8 mb-8'>
-      <div className='relative overflow-hidden shadow-md pb-80 mb-6'>
-        <img
-          src={post.featuredImage.url}
-          alt={post.title}
-          className='object-center absolute h-full w-full object-cover shadow-lg rounded-t-lg lg:rounded-lg'
-        />
+      <div className='relative overflow-hidden mb-6'>
+        <Link href={`/post/${post.slug}`} passHref>
+          <img
+            src={post.featuredImage.url}
+            alt={post.title}
+            className='cursor-pointer max-h-[600px] w-full object-bottom object-cover shadow-lg rounded-t-lg xl:max-h-[900px] lg:min-h-full lg:rounded-lg'
+          />
+        </Link>
       </div>
       <h1 className='transition duration-500 text-center mb-4 cursor-pointer hover:text-blue-500 text-xl font-semibold'>
         <Link href={`/post/${post.slug}`} passHref>
