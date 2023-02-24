a5f7a3e9e57de1fbc371e9fd45561efa1bacc575
Maksym Sagadin <maxsagadin@gmail.com>
2023-02-24 12:30:06 -0800

    realigned classnames for easier to read logic

--- pages/blog.js
+++ pages/blog.js
@@ -9,14 +9,14 @@ export default function Home({ posts, contact, featuredPosts }) {
     <div className="px-4 mt-28 sm:px-8 lg:mb-10">
       <Header logoURL={contact.logo.url} />
       <FeaturedPosts posts={featuredPosts} />
-      <div className='grid lg:grid-cols-12 grid-cols-1 lg:gap-8'>
+      <div className='grid grid-cols-1 lg:grid-cols-12 lg:gap-8'>
         {/* Left Section */}
-        <div className='lg:col-span-8 col-span-1'>
+        <div className='col-span-1 lg:col-span-8'>
           {posts.map((post) => <PostCard post={post} key={post.title} />)}
         </div>
         {/* Right Section */}
         <div className='col-span-1 lg:col-span-4'>
-            <div className='lg:sticky relative text-gray-200'>
+            <div className='relative text-gray-200 lg:sticky'>
               <Categories />
               <PostWidget />
             </div>
