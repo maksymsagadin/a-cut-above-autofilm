28d5fc6db41b8e6835bbce166652156151937963
Maksym Sagadin <maxsagadin@gmail.com>
2023-03-13 15:14:48 -0700

    updated blog page to filter posts based on categories

--- pages/blog.js
+++ pages/blog.js
@@ -1,29 +1,63 @@
-import Head from 'next/head'
-import { Header, Footer, PostCard, Categories, CategoriesFilter, PostWidget } from '../components'
+import { useState, useCallback, useMemo } from 'react'
+import { Header, Footer, PostCard, CategoriesFilter} from '../components'
 import { getBlog } from '../services'
 import { FeaturedPosts } from '../sections'
 import { SectionHeadline } from '../components'
 
 
 export default function Home({ posts, categories, contact, featuredPosts }) {
-  // Sorting Categories Alphabetically
-  categories.sort(function(a,b) {
-    let nameA = a.name.toLowerCase()
-    let nameB = b.name.toLowerCase()
-    if (nameA < nameB) return -1
-    if (nameA > nameB) return 1
-    return 0
+  const [Posts, setPosts] = useState({
+    filteredPosts: posts,
+    filters: new Set(),
   })
-  
+
+  const handleFilterChange = useCallback(event => {
+    console.log(event.target.value, event.target.checked)
+    setPosts(previousState => {
+      let filters = new Set(previousState.filters)
+      let filteredPosts = posts
+
+      // Update Filter list with checked values
+      if (event.target.checked) {
+        filters.add(event.target.value)
+      } else {
+        filters.delete(event.target.value)
+      }
+      // Filter posts based on filters
+      if (filters.size) {
+        filteredPosts = filteredPosts.filter(post => {
+          for (const filter of filters) {
+            if (!post.categories.some(category => category.name === filter)) {
+              return false
+            }
+          }
+          return true
+        })
+      }
+      return { filteredPosts, filters }
+    })
+  }, [setPosts]);
+
+  // Sorting Categories Alphabetically
+  const sortedCategories = useMemo(() => {
+    return categories.sort(function(a,b) {
+      let nameA = a.name.toLowerCase()
+      let nameB = b.name.toLowerCase()
+      if (nameA < nameB) return -1
+      if (nameA > nameB) return 1
+      return 0
+    })
+  }, [categories])
+
   return (
     <div className="mt-28 lg:mb-10">
       <Header logoURL={contact.logo.url} />
       <FeaturedPosts posts={featuredPosts} />
       <SectionHeadline title='Blog'>
         {/* <Categories /> */}
-        <CategoriesFilter categories={categories} />
+        <CategoriesFilter categories={categories} onFilterChange={handleFilterChange} />
         <div className='md:columns-2 lg:columns-3 gap-4 lg:gap-8'>
-          {posts.map((post) => 
+          {Posts.filteredPosts.map((post) => 
             <div key={post.title} className='break-inside-avoid'>
               <PostCard post={post} />
             </div>
@@ -38,6 +72,11 @@ export default function Home({ posts, categories, contact, featuredPosts }) {
 export async function getStaticProps() {
   const blogData = (await getBlog()) || []
   return {
-    props: { posts: blogData.posts, categories: blogData.categories, contact: blogData.data.contact, featuredPosts: blogData.postsConnection.edges }
+    props: { 
+      posts: blogData.posts,
+      categories: blogData.categories,
+      contact: blogData.data.contact,
+      featuredPosts: blogData.postsConnection.edges
+    }
   }
 }
