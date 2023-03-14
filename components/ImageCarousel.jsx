cf4f2dedab6c0a1dfccc9251df1e6ec36724d967
Maksym Sagadin <maxsagadin@gmail.com>
2023-03-14 14:32:01 -0700

    updated carousel to slide 2 slides when more then 2 items visible

--- components/ImageCarousel.jsx
+++ components/ImageCarousel.jsx
@@ -6,14 +6,17 @@ const responsive = {
     extremeLargeDesktop: {
         breakpoint: { max: 4015, min: 2515 },
         items: 5,
+        slidesToSlide: 2,
     },
     superLargeDesktop: {
       breakpoint: { max: 2515, min: 1915 },
       items: 4,
+      slidesToSlide: 2,
     },
     largeDesktop: {
         breakpoint: { max: 1915, min: 1315 },
         items: 3,
+        slidesToSlide: 2,
     },
     desktop: {
       breakpoint: { max: 1315, min: 1023 },
@@ -22,22 +25,31 @@ const responsive = {
     largeTablet: {
         breakpoint: { max: 1023, min: 815 },
         items: 4,
+        slidesToSlide: 2,
     },
     tablet: {
       breakpoint: { max: 815, min: 530 },
       items: 3,
+      slidesToSlide: 2,
     },
     mobile: {
       breakpoint: { max: 530, min: 0 },
       items: 2,
-      slidesToSlide: 1,
     },
   }
 
 const ImageCarousel = ({ posts }) => {
     return (
         <>
-            <Carousel infinite={true} autoPlay={true} autoPlaySpeed={3000} partialVisbile={true} pauseOnHover={true} customTransition={'transform 1000ms ease-in-out'} transitionDuration={2000} arrows={true} responsive={responsive} >
+            <Carousel 
+            infinite={true} 
+            autoPlay={true} 
+            autoPlaySpeed={4000}
+            removeArrowOnDeviceType={['mobile','tablet','largeTablet']} 
+            pauseOnHover={true} customTransition={'transform 800ms ease-in-out'} 
+            transitionDuration={2000} 
+            arrows={true} 
+            responsive={responsive} >
                 {posts && posts.map((post, index) => (
                     <FeaturedImage key={index} post={post} />
                 ))}
