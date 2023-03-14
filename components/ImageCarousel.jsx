2b75fdd6ba398a2b7ef18233cbedf5300c7ac85a
Maksym Sagadin <maxsagadin@gmail.com>
2023-03-14 14:05:57 -0700

    created ImageCarousel  component with responsive sizing

---
+++ components/ImageCarousel.jsx
@@ -0,0 +1,49 @@
+import Carousel from 'react-multi-carousel'
+import 'react-multi-carousel/lib/styles.css'
+import FeaturedImage from './FeaturedImage'
+
+const responsive = {
+    extremeLargeDesktop: {
+        breakpoint: { max: 4015, min: 2515 },
+        items: 5,
+    },
+    superLargeDesktop: {
+      breakpoint: { max: 2515, min: 1915 },
+      items: 4,
+    },
+    largeDesktop: {
+        breakpoint: { max: 1915, min: 1315 },
+        items: 3,
+    },
+    desktop: {
+      breakpoint: { max: 1315, min: 1023 },
+      items: 2,
+    },
+    largeTablet: {
+        breakpoint: { max: 1023, min: 815 },
+        items: 4,
+    },
+    tablet: {
+      breakpoint: { max: 815, min: 530 },
+      items: 3,
+    },
+    mobile: {
+      breakpoint: { max: 530, min: 0 },
+      items: 2,
+      slidesToSlide: 1,
+    },
+  }
+
+const ImageCarousel = ({ posts }) => {
+    return (
+        <div className="mb-4 lg:mb-8">
+            <Carousel infinite={true} autoPlay={true} autoPlaySpeed={3000} partialVisbile={true} pauseOnHover={true} customTransition={'transform 1000ms ease-in-out'} transitionDuration={2000} arrows={true} responsive={responsive} >
+                {posts && posts.map((post, index) => (
+                    <FeaturedImage key={index} post={post} />
+                ))}
+            </Carousel>
+        </div>
+    )
+}
+
+export default ImageCarousel
