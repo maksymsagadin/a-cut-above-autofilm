84cf0ca622c646fdb40176e4abf09fd07202e148
Maksym Sagadin <maxsagadin@gmail.com>
2023-03-13 22:04:23 -0700

    update to visually see text easier

--- components/FeaturedPostCard.jsx
+++ components/FeaturedPostCard.jsx
@@ -1,17 +1,16 @@
 import React from 'react'
 import moment from 'moment'
-import Image from 'next/image'
 import Link from 'next/link'
 
 const FeaturedPostCard = ({ post }) => (
   <div className="relative h-96">
-    <div className="absolute rounded-lg bg-center bg-no-repeat bg-cover shadow-md inline-block w-full h-96" style={{ backgroundImage: `url('${post.node.featuredImage.url}')` }} />
+    <div className="absolute rounded-lg bg-center bg-no-repeat bg-cover shadow-md inline-block w-full h-96" style={{ backgroundImage: `url('${post.featuredImage.url}')` }} />
     <div className="flex flex-col rounded-lg p-6 items-end justify-end absolute w-full h-full">
-        <p className="text-white text-shadow font-semibold text-sm">{moment(post.node.createdAt).format('MMM DD, YYYY')}</p>
-        <p className="text-white text-shadow font-semibold text-sm">{post.node.title}</p>
+        <p className="text-white text-shadow font-semibold text-sm p-1 rounded-t bg-yellow-600 bg-opacity-70">{moment(post.createdAt).format('MMM DD, YYYY')}</p>
+        <p className="text-white text-shadow font-semibold text-sm p-1 rounded rounded-tr-none bg-yellow-600 bg-opacity-70">{post.title}</p>
     </div>
     <div className="absolute rounded-lg bg-center bg-gradient-to-b opacity-20 hover:bg-none from-gray-400 via-gray-700 to-black w-full h-96">
-        <Link href={`/post/${post.node.slug}`} passHref><span className="cursor-pointer absolute w-full h-full" /></Link>
+        <Link href={`/post/${post.slug}`} passHref><span className="cursor-pointer absolute w-full h-full" /></Link>
     </div>
   </div>
 );
