6656204d1146298b69dbf18cd900c9faaf24d491
Maksym Sagadin <maxsagadin@gmail.com>
2023-03-13 23:01:09 -0700

    update checkbox color when checked

--- components/CategoriesFilter.jsx
+++ components/CategoriesFilter.jsx
@@ -7,7 +7,7 @@ const CategoriesFilter = ({ categories, onFilterChange }) => {
             <ul className='columns-2 sm:columns-3 items-start lg:flex lg:justify-evenly gap-4'>
                 {categories.map((category) => (
                     <li key={category.name} className='flex lg:flex-col items-center py-1 md:p-2'>
-                        <input type='checkbox' onChange={onFilterChange} id={category.name} name={category.name} value={category.name} className='w-4 h-4 text-yellow-400 bg-gray-100 border-gray-300 rounded focus:ring-yellow-500 dark:focus:ring-yellow-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600'/>
+                        <input type='checkbox' onChange={onFilterChange} id={category.name} name={category.name} value={category.name} className='w-4 h-4 rounded checked:accent-yellow-400'/>
                         <label htmlFor={category.name} className='px-2 lg:pt-2 text-center'>{category.name}</label>
                     </li>
                 ))}
