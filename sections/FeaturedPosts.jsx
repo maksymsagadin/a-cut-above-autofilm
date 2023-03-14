40182000d5b7e5b9afd1555c6b406afb468eb107
Maksym Sagadin <maxsagadin@gmail.com>
2023-03-14 14:07:39 -0700

    small padding update for mobile and desktop responsiveness

--- sections/FeaturedPosts.jsx
+++ sections/FeaturedPosts.jsx
@@ -26,7 +26,7 @@ const responsive = {
 
 const FeaturedPosts = ({ posts }) => {
   return (
-    <div className="mb-8">
+    <div className="mb-4 lg:mb-8">
       <Carousel infinite={true} autoPlay={true} autoPlaySpeed={4000} customTransition={'transform 2000ms ease-in-out'} transitionDuration={2000} arrows={true}  responsive={responsive} itemClass="px-4" >
         {posts && posts.map((post, index) => (
           <FeaturedPostCard key={index} post={post} />
