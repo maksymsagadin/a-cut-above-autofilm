d2450e664dc2fb74610bea186c39dd7f03fdd65b
Maksym Sagadin <maxsagadin@gmail.com>
2023-03-15 22:46:58 -0700

    added z-index:10 to div container and position:absolute to <p> tag

--- components/FeaturedImage.jsx
+++ components/FeaturedImage.jsx
@@ -5,9 +5,9 @@ import Image from 'next/image'
 const FeaturedImage = ({ post }) => {
   return (
     <Link href={`/post/${post.slug}`} passHref >
-        <div className='cursor-pointer w-[250px] h-[400px] lg:w-[400px] lg:h-[550px] text-white' >
+        <div className='cursor-pointer z-10 w-[250px] h-[400px] lg:w-[400px] lg:h-[550px]'>
           <Image src={post.featuredImage.url} alt={post.title} layout='fill' objectFit='cover'/>
-          <p className='cursor-pointer font-semibold h-full w-full flex items-end p-2 opacity-0 hover:bg-opacity-30 hover:opacity-90 bg-black'>{post.title}</p>
+          <p className='cursor-pointer font-semibold absolute h-full w-full flex items-end p-2 opacity-0 hover:bg-opacity-30 hover:opacity-90 bg-black'>{post.title}</p>
         </div>
     </Link>
   )
